35a0910eed124bc2622c40a5ea7789d945c5e412
voloshkin-v <voloshin.v.02@gmail.com>
2021-04-30 16:17:45 +0300

    added functionality to the component SortPopup

--- front/src/App.js
+++ front/src/App.js
@@ -10,7 +10,7 @@ function App() {
         		<div className="container">
           			<div className="content__top">
 						<Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']} />
-						<SortPopup/>
+						<SortPopup />
           			</div>
           		<h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
