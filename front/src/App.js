7b73811ded0058a05c378ebddb422e059ab39ae7
voloshkin-v <voloshin.v.02@gmail.com>
2021-04-30 19:41:53 +0300

    added fake db.json and created new component PizzaBlock

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
-
-import {Header} from './components';
-import {Home, Cart} from './pages';
 import {Route} from 'react-router-dom';
 
+import {Header} from './components';
+
+import {Home, Cart} from './pages';
+
 function App() {
+	const [pizzas, setPizzas] = React.useState([])
+
+	React.useEffect(() => {
+		fetch('http://localhost:3000/db.json')
+			.then(response => response.json())
+			.then(json => {
+				setPizzas(json.pizzas);
+			});
+	}, []);	
+
   	return (
 		<div className="wrapper">
       		<Header />
       		<div className="content">
-			  	<Route path='/' component={Home} exact />
+			  	<Route path='/' render={() => <Home items={pizzas} />} exact />
 			  	<Route path='/cart' component={Cart} exact />
-
       		</div>
     	</div>
   	);
--- front/src/components/index.js
+++ front/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header';
 import Categories from './Categories';
 import SortPopup from './SortPopup';
+import PizzaBlock from './PizzaBlock';
 
-export {Header, Categories, SortPopup};
+export {Header, Categories, SortPopup, PizzaBlock};
