3763a8168fc268c166b8187c51c4997fbcf52fcc
voloshkin-v <voloshin.v.02@gmail.com>
2021-04-30 14:39:34 +0300

    fixed undefined bug on Component Categories

--- front/src/components/Categories.js
+++ front/src/components/Categories.js
@@ -1,40 +1,5 @@
 import React from 'react'
 
-// class Categories extends React.Component {
-//     state = { 
-//         activeItem: 3
-//     };
-
-//     onSelectItem = index => {
-//         this.setState({
-//             activeItem: index
-//         });
-//     }
-
-    
-
-//     render() {
-//         const {items} = this.props;
-
-//         return (
-//             <div className="categories">    
-//                 <ul>
-//                     <li>Все</li>
-//                     {items.map((item, index) => (
-//                         <li 
-//                             className={this.state.activeItem === index ? 'active' : ''} 
-//                             onClick={() => this.onSelectItem(index)} 
-//                             key={`${item}_${index}`}>
-//                             {item}
-//                         </li>
-//                     ))}
-//                 </ul>
-//             </div>
-//         )
-//     }
-// }
-
-
 function Categories({items}) {
     const [activeItem, setActiveItem] = React.useState(null);
 
@@ -46,7 +11,7 @@ function Categories({items}) {
         <div className="categories">
             <ul>
                 <li className={activeItem === null ? 'active' : ''} onClick={() => onSelectItem(null)}>Все</li>
-                {items.map((item, index) => (
+                {items && items.map((item, index) => (
                     <li 
                         className={activeItem === index ? 'active' : ''} 
                         onClick={() => onSelectItem(index)} 
